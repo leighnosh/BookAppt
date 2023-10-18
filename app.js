523a7527fdc59f427096992e42757b66786a145e
leighnosh <112686904+leighnosh@users.noreply.github.com>
2023-10-18 14:21:29 +0530

    Axios DELETE
    Delete functionality added

--- app.js
+++ app.js
@@ -23,8 +23,6 @@ function postUserData(event) {
         });
 }
 
-
-
 function getUserData() {
     var appointmentsDiv = document.getElementById('appointments');
     appointmentsDiv.innerHTML = '';
@@ -41,8 +39,8 @@ function getUserData() {
                         <p>Name: ${appointment.name}</p>
                         <p>Email: ${appointment.email}</p>
                         <p>Phone: ${appointment.phone}</p>
-                        <button onclick="editAppointment('${appointment.email}', ${index})">Edit</button>
-                        <button onclick="deleteAppointment('${appointment.email}', ${index})">Delete</button>
+                        <button onclick="editAppointment('${appointment._id}', ${index})">Edit</button>
+                        <button onclick="deleteAppointment('${appointment._id}', ${index})">Delete</button>
                     `;
 
                     appointmentsDiv.appendChild(appointmentDiv);
@@ -51,5 +49,19 @@ function getUserData() {
         })
         .catch(function(error) {
             console.error('Error fetching appointments:', error);
+        });
+}
+
+
+function deleteAppointment(id, index) {
+    axios.delete(`${apiUrl}/${id}`)
+        .then(function(response) {
+            alert('User data deleted successfully!');
+            var appointmentsDiv = document.getElementById('appointments');
+            var appointmentToRemove = appointmentsDiv.getElementsByClassName('appointment')[index];
+            appointmentsDiv.removeChild(appointmentToRemove);
+        })
+        .catch(function(error) {
+            console.error('Error deleting appointment:', error);
         });
 }
